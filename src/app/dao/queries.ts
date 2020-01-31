e576e0dfde71a0a873fae29e00fbd7bf499da89c
maucrvlh <mauricio.marinho@tjam.jus.br>
2020-01-31 17:19:54 -0400

    ajustes gerais

--- src/app/dao/queries.ts
+++ src/app/dao/queries.ts
@@ -17,24 +17,10 @@ export default {
         pg: {
             select: {
                 queryDadosDoProcesso: `
-                    SELECT P.VLCAUSA "valor", P.CDPROCESSO "codprocesso", P.CDPARTEATIVAPRINC "ativoprincipal", P.CDPARTEPASSIVPRINC "passivoprincipal", P.NUPROCESSO "processo", NOME.CDPESSOA "codparte", NOME.NMPESSOA "nome", TIPOPARTE.DECOMPTIPOPARTE "participacao", PESSOA.NUIDADE "idade", PARTE.FLSEGREDOJUSTICA "segredojustica", PARTE.TPPARTE "parte", PARTE.NUSEQPARTE "seqparte", PARTE.NUSEQPARTEREFER "seqparterefer",
-                    PDOCCPF.NUDOCFORMATADO "cpf", PDOCOAB.NUDOCFORMATADO "oab", PDOCCNPJ.NUDOCFORMATADO "cnpj"
-                    FROM SAJ.EFPGPROCESSO P 
-                    INNER JOIN SAJ.EFPGPARTE PARTE ON PARTE.CDPROCESSO = P.CDPROCESSO 
-                    INNER JOIN SAJ.ESAJNOME NOME ON  PARTE.CDPESSOA = NOME.CDPESSOA AND NOME.TPNOME = 'N'  
-                    INNER JOIN SAJ.ESAJTIPOPARTE TIPOPARTE ON TIPOPARTE.CDTIPOPARTE = PARTE.CDTIPOPARTE 
-                    INNER JOIN SAJ.ESAJPESSOA PESSOA ON PESSOA.CDPESSOA = PARTE.CDPESSOA 
-                    LEFT OUTER JOIN SAJ.ESAJPESSOADOC PDOCOAB ON PDOCOAB.CDPESSOA = PARTE.CDPESSOA AND PDOCOAB.SGTIPODOCUMENTO = 'OAB'
-                    LEFT OUTER JOIN SAJ.ESAJPESSOADOC PDOCCPF ON PDOCCPF.CDPESSOA = PARTE.CDPESSOA AND PDOCCPF.SGTIPODOCUMENTO = 'CPF'
-                    LEFT OUTER JOIN SAJ.ESAJPESSOADOC PDOCCNPJ ON PDOCCNPJ.CDPESSOA = PARTE.CDPESSOA AND PDOCCNPJ.SGTIPODOCUMENTO = 'CNPJ'
-                    WHERE 
-                        P.FLTIPOCLASSE NOT IN (5) AND 
-                        P.NUPROCESSO = ':processo'
-                    ORDER BY PARTE.TPPARTE, PARTE.NUSEQPARTE, PARTE.NUSEQPARTEREFER DESC
                     `,
 
                 queryGetBlob: `
-                    SELECT * FROM SAJ.EDIGIMAGEMDOC WHERE CDUSUINCLUSAO=':usuario' AND CDDOCUMENTO=':doc'
+                    
                     `
             },
             insert: {},
@@ -58,7 +44,7 @@ export default {
         cnjbrasil: {
             select: {
                 queryDadosProcesso: `                
-                    select * from cnjbrasil.processo where numeroprocesso=':processo'
+                    
                 `
             },
             insert: {},
